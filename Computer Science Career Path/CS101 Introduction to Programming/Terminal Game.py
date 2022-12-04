fda4f2d543afbcdf9b57a48c9fd208e6ff42e490
Lyle Wong <lylereturns@gmail.com>
2022-12-04 22:57:27 +0000

    Changes made: - Adjusted difficulty of the game (modified status attributes) - Added more functionality to the status attributes - Added 3 ending possibilities - Added separation text to each print statement - Removed temporary notes at the top of the code.

--- Computer Science Career Path/CS101 Introduction to Programming/Terminal Game.py
+++ Computer Science Career Path/CS101 Introduction to Programming/Terminal Game.py
@@ -1,19 +1,6 @@
 # Import modules
 import random
 
-"""
-Survive the night game 
-
-- There will be a hunger, hydration and fatigue bar
-- Stay warm (stoke the fire) (reduces fatigue and lowers danger)
-- Stay awake to avoid danger
-- Danger bar increases as fatigue bar increases, wild animals can smell you.
-- Use random module to randomly decide whether an item break
-- Instead of time, the player needs to get some goals achieved before they run out of turns, otherwise they've lost
-
-"""
-
-
 class Player:
     def __init__(self, hunger, fatigue, hydration):
 
@@ -55,8 +42,9 @@ class Player:
             # Increase hydration slightly (The maximum value is 100)
             self.hydration = min(self.hydration + 10, 100)
             # Display message
+            print("-------------------------------------------------------------------------------------------------")
             print(f'0.25 litres of {self.inventory["drinks"][drink_item_index][0]} has been consumed! Fatigue is now {self.fatigue}! Hydration is now at {self.hydration}! You are feeling refreshed!')
-
+            print("-------------------------------------------------------------------------------------------------")
 
         # If the drink was water
         elif drink_item_index == 1:
@@ -65,8 +53,9 @@ class Player:
             # Increase hydration (The maximum value is 100)
             self.hydration = min(self.hydration + 30, 100)
             # Display message
+            print("-------------------------------------------------------------------------------------------------")
             print(f'0.5 litres of {self.inventory["drinks"][drink_item_index][0]} has been consumed! Hydration is now {self.hydration}! That hit the spot..')
-    
+            print("-------------------------------------------------------------------------------------------------")
 
     def eat_food(self, food_item_index):
         # Decrement the quantity of the food
@@ -76,7 +65,9 @@ class Player:
         self.hunger = max(0, self.hunger - 30)
 
         # Display that the food item has been eaten
+        print("-------------------------------------------------------------------------------------------------")
         print(f'{self.inventory["food"][food_item_index][0]} has been eaten! Hunger is now {self.hunger}!')
+        print("-------------------------------------------------------------------------------------------------")
 
     def light_fire(self, tool_item_index):
         # 20 % chance of breaking
@@ -88,14 +79,18 @@ class Player:
         if tool_item_index == 0:
             # If the random number is 3, then the matchstick broke
             if matchstick_random_number == 3: # 3 = random selected number
+                print("-------------------------------------------------------------------------------------------------")
                 print(f'The {self.inventory["tools"][tool_item_index][0]} broke!')
+                print("-------------------------------------------------------------------------------------------------")
 
             # If it isn't then the fire was successfully lit
             else:
                 # Decrease danger (The minimum value is 0)
-                self.danger = max(0, self.danger - 10)
+                self.danger = max(0, self.danger - 25)
                 # Display message
+                print("-------------------------------------------------------------------------------------------------")
                 print(f'The {self.inventory["tools"][tool_item_index][0]} successfully lit a fire! With light you feel more comfortable! Danger is now at {self.danger}')
+                print("-------------------------------------------------------------------------------------------------")
 
             # Decrement the quantity of the tool (regardless of whether it was successful or not)
             self.inventory["tools"][tool_item_index][1] -= 1
@@ -104,16 +99,20 @@ class Player:
         if tool_item_index == 1:
             # If the random number is 9, then the lighter broke
             if lighter_random_number == 9: # 9 = random selected number
+                print("-------------------------------------------------------------------------------------------------")
                 print(f'The {self.inventory["tools"][tool_item_index][0]} stopped working!')
+                print("-------------------------------------------------------------------------------------------------")
                 # Decrement the quantity of the tool as it is no longer working
                 self.inventory["tools"][tool_item_index][1] -= 1
 
             # If it isn't then the fire was successfully lit
             else:
                 # Decrease danger (The minimum value is 0)
-                self.danger = max(0, self.danger - 10)
+                self.danger = max(0, self.danger - 25)
                 # Display message
+                print("-------------------------------------------------------------------------------------------------")
                 print(f'The {self.inventory["tools"][tool_item_index][0]} successfully lit a fire! With light you feel more comfortable! Danger is now at {self.danger}')
+                print("-------------------------------------------------------------------------------------------------")
 
 
     # Support
@@ -185,7 +184,9 @@ class Player:
                             # Take a turn away from the player
                             self.turns -= 1
                         else:
+                            print("-------------------------------------------------------------------------------------------------")
                             print(f'There are no more {self.inventory["food"][0][0]}es!')
+                            print("-------------------------------------------------------------------------------------------------")
 
 
                     # Pizza slices
@@ -199,7 +200,9 @@ class Player:
                             # Take a turn away from the player
                             self.turns -= 1
                         else:
+                            print("-------------------------------------------------------------------------------------------------")
                             print(f'There are no more {self.inventory["food"][1][0]}!')
+                            print("-------------------------------------------------------------------------------------------------")
 
                     # Go back
                     case "3":
@@ -226,7 +229,9 @@ class Player:
                             # Take a turn away from the player
                             self.turns -= 1          
                         else:
+                            print("-------------------------------------------------------------------------------------------------")
                             print(f'There is no more {self.inventory["drinks"][0][0]}!')
+                            print("-------------------------------------------------------------------------------------------------")
 
                     # Water
                     case "2":  
@@ -240,7 +245,9 @@ class Player:
                             # Take a turn away from the player
                             self.turns -= 1
                         else:
+                            print("-------------------------------------------------------------------------------------------------")
                             print(f'There is no more {self.inventory["drinks"][1][0]}!')
+                            print("-------------------------------------------------------------------------------------------------")
 
                     # Go back
                     case "3":
@@ -266,7 +273,9 @@ class Player:
                             # Take a turn away from the player
                             self.turns -= 1          
                         else:
+                            print("-------------------------------------------------------------------------------------------------")
                             print(f'You broke all the {self.inventory["tools"][0][0]}s...')
+                            print("-------------------------------------------------------------------------------------------------")
 
                     # Lighter
                     case "2":  
@@ -279,7 +288,9 @@ class Player:
                             # Take a turn away from the player
                             self.turns -= 1
                         else:
+                            print("-------------------------------------------------------------------------------------------------")
                             print(f'The {self.inventory["tools"][1][0]} stopped working!')
+                            print("-------------------------------------------------------------------------------------------------")
 
                     # Go back
                     case "3":
@@ -302,11 +313,70 @@ class Player:
                 choosing_food = False
                 choosing_drink = False
 
+                # Check the current stats of the player
+
+                # HUNGER
+                if self.hunger > 50: # Early warning
+                    # Display message
+                    print("-------------------------------------------------------------------------------------------------")
+                    print("You're becoming nauseous from a lack of food. You might want to eat some food!")
+                    print("-------------------------------------------------------------------------------------------------")
+
+                if self.hunger == 100: # Effecting player's fatigue status
+                    # Increase the players fatigue
+                    self.fatigue = min(self.fatigue + 20, 100)
+                    # Display message
+                    print("-------------------------------------------------------------------------------------------------")
+                    print(f"You feel sluggish, you might want to eat something. Fatigue has increased to {self.fatigue}!")
+                    print("-------------------------------------------------------------------------------------------------")
+
+                # FATIGUE
+                if self.fatigue > 50:
+                    # Increase the danger level
+                    self.danger = min(self.danger + 10, 100)
+                    # Display message
+                    print("-------------------------------------------------------------------------------------------------")
+                    print("Try and wake yourself up! You definitely don't want to fall asleep out here... They can smell it..")
+                    print("-------------------------------------------------------------------------------------------------")
+                    print(f"Your insticts are telling you that something's wrong... Danger has increased to {self.danger}")
+                    print("-------------------------------------------------------------------------------------------------")
+
+                if self.fatigue == 100:
+                    break
+
+                # DANGER
+
+                if self.danger > 50:
+                    print("-------------------------------------------------------------------------------------------------")                   
+                    print("Whats that sound?...")
+                    print("-------------------------------------------------------------------------------------------------")
+
+                if self.danger == 100:
+                    break
+
+                
                 # Display the players' current stats
                 print("-------------------------------------------------------------------------------------------------")
                 print(f"Hunger:{self.hunger}, Hydration:{self.hydration}, Fatigue:{self.fatigue}, Danger: {self.danger}, Turns remaining: {self.turns}")
                 print("-------------------------------------------------------------------------------------------------")
-    
+
+        # ENDING POSSIBILITIES
+
+        # If the player fell asleep i.e. the fatigue reached its maximum capacity
+        if self.fatigue == 100:
+            print("-------------- YOU LOST --------------")
+            print("You fell asleep and were mauled by the wild animals of the woods..")
+
+        # If the danger meter reached its maximum capacity
+        elif self.danger == 100:
+            print("-------------- YOU LOST --------------")
+            print("A wild grizzly bear charges towards you and mangles you to death...")
         
+        # If neither of the above occurred
+        else:
+            print("-------------- YOU WON --------------")
+            print("You survived the night! Congratulations!")
+
+
 
 player = Player(hunger = 0, hydration = 100, fatigue = 30)
